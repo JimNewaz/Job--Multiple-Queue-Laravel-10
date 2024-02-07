395e6b6521ecbe9bb454a0d1a8f07c5050a46cac
JimNewaz <miznewaz@gmail.com>
2024-02-07 11:48:24 +0600

    Log and Reg

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,27 @@
 <?php
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\Auth\LoginRegisterController;
 
-use App\Http\Controllers\LoginRegisterController;
-use Illuminate\Support\Facades\Route;
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::controller(LoginRegisterController::class)->group(function() {
-    Route::get('/register', 'register')->name('register');
-    Route::post('/store', 'store')->name('store');
-    Route::get('/login', 'login')->name('login');
-    Route::post('/authenticate', 'authenticate')->name('authenticate');
-    Route::get('/dashboard', 'dashboard')->name('dashboard');
-    Route::post('/logout', 'logout')->name('logout');
-});
+    Route::controller(LoginRegisterController::class)->group(function() {
+        Route::get('/register', 'register')->name('register');
+        Route::post('/store', 'store')->name('store');
+        Route::get('/login', 'login')->name('login');
+        Route::post('/authenticate', 'authenticate')->name('authenticate');
+        Route::get('/dashboard', 'dashboard')->name('dashboard');
+        Route::post('/logout', 'logout')->name('logout');
+    });
